6ddc259044e6b58acd7bdc79f32077de1cbe41ee
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2023-01-25 23:19:57 +0100

    finishing the api integration

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 
 function App() 
 {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
   const configuration = new Configuration({
     apiKey: import.meta.env.VITE_Open_AI_Key,
   });
@@ -12,16 +14,26 @@ function App()
   const generateImage = async () =>{
     console.log("generating...");
     const res = await openai.createImage({
-      prompt: "titan vs spiderman",
+      prompt: prompt,
       n: 1,
       size: "1024x1024",
     });
-    console.log(res.data.data[0].url);
+    setResult(res.data.data[0].url);
   };
 
   return (
-    <div>
+    <div className='app-main'>
+      <h2>Text To Image</h2>
+      <input className='app-input' placeholder='Type text to generate to image' type='text' onChange={(e)=> setPrompt(e.target.value)}/>
       <button onClick={generateImage}>Generate Image</button>
+      
+      {
+        result.length > 0 ? 
+        <img className='app-image' src={result} alt={prompt} /> 
+        :
+        <></>
+      }
+      
     </div>
   );
 }
